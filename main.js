5e02fc7af51ba34df97c7314a6765478d5a9cbb3
just-maik <m.j1854s@googlemail.com>
2022-10-10 11:52:44 +0200

    we got gitignore now

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import './style.scss'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
@@ -8,9 +7,9 @@ document.querySelector('#app').innerHTML = `
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      <img src="/javascript.svg" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello CUNT!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
