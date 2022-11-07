ee2d785e889e37d50a3ba45c638f23b48f7cf78f
just-maik <maik.jonietz@googlemail.com>
2022-11-07 09:40:19 +0000

    st

--- composables/useUsers.ts
+++ composables/useUsers.ts
@@ -1,18 +1,24 @@
-import { GetUserByIdQuery } from "#gql";
+
+import { GetUserByUsernameQuery } from "#gql";
 import { TypeFromQuery } from "~~/helper/typeFromArray";
 7;
-export type UserType = TypeFromQuery<GetUserByIdQuery["appusers"]>;
+export type UserType = TypeFromQuery<GetUserByUsernameQuery["appusers"]>;
 
 export const useUsers = async () => {
-	
-	const data = (await useAsyncData(() => GqlGetUserById({ id: "Udo" }))).data
-		.value?.appusers as UserType[];
-	return data;
+	return new UserService();
 };
 
-class UserService{
-	constructor(){
+class UserService {
+	constructor() {}
+	public async getUser(username: string): Promise<UserType[]> {
+		const data = (await useAsyncData(() => GqlGetUserByUsername()))
+			.data.value?.appusers as UserType[];
+		return data;
+	}
+	public checkLogin(username: string, password: string) {
+		
+	}
+	public createUser(username: string, password: string) {
 
 	}
-	publ
 }
