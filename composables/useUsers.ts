cac9732d56287fd041db5bff7b7bb6fe010a8892
just-maik <maik.jonietz@googlemail.com>
2022-11-21 09:07:31 +0000

    fixing silas shit error

--- composables/useUsers.ts
+++ composables/useUsers.ts
@@ -1,15 +1,15 @@
-import { GetUserByUsernameQuery } from "#gql";
+import { GetUserByUsernameQuery, LoginQuery } from "#gql";
 import { sha3_256 } from "js-sha3";
 import { TypeFromQuery } from "~~/helper/typeFromArray";
 
-export type UserType = TypeFromQuery<GetUserByUsernameQuery["appusers"]>;
+export type UserType = GetUserByUsernameQuery['appusers'];
 
 export const useUsers = async () => {
 	return new UserService();
 };
 
 class UserService {
-	constructor() {}
+	constructor() { }
 	public async getUser(username: string): Promise<UserType[]> {
 		const data = (
 			await useAsyncData(() => GqlGetUserByUsername({ username: username }))
@@ -24,9 +24,10 @@ class UserService {
 			await useAsyncData(() =>
 				GqlLogin({ username: username, password: sha3_256(password) })
 			)
-		).data.value?.appusers as UserType[];
+		).data.value?.appusers;
+		console.log(data);
 
-		return Array.isArray(data) ? true : false;
+		return Array.isArray(data) && data.length > 0 ? true : false;
 	}
-	public createUser(username: string, password: string) {}
+	public createUser(username: string, password: string) { }
 }
