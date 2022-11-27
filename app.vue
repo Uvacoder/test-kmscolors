e0092b918aacbe297fc04a5bf0ddf7d2935fdacc
just-maik <maik.jonietz@googlemail.com>
2022-11-27 19:16:41 +0000

    fkn  ihjqbaeobnOJ

--- app.vue
+++ app.vue
@@ -7,3 +7,7 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+const auth = await useAuth().tryContinueSession();
+</script>
--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,42 +1,89 @@
-import { GetUserByMailQuery } from "#gql";
+import { GetUserByMailQuery, LoginMutation } from "#gql";
 
-export type AuthData = {
+export type AuthData = LoginMutation["auth_login"] & {
 	currentUser?: GetUserByMailQuery["users"][number];
-	accessToken?: string;
-	refreshToken?: string;
-	tokenExpiration?: number;
 };
 
 class AuthService {
-	private data = useState<AuthData>("authData", () => {return {} });
+	private refreshTokenCookie = useCookie("__rftkn", {
+		sameSite: "strict",
+		secure: true,
+	});
+	private authTokenCookie = useCookie("__autkn", {
+		sameSite: "strict",
+		secure: true,
+	});
+
+	private lastUserCookie = useCookie("__lusrc", {
+		sameSite: "strict",
+		secure: true,
+	});
+
+	private data = useState<AuthData>("authData", () => {
+		return {};
+	});
+
 	public async sendLoginRequest(email: string, password: string) {
 		try {
 			let res = await GqlLogin({ email: email, password: password });
-
-			this.data.value = {
-				accessToken: res.auth_login?.access_token!,
-                refreshToken: res.auth_login?.refresh_token!,
-                tokenExpiration: res.auth_login?.expires!,
-                currentUser: (await GqlGetUserByMail({ email: email })).users?.[0]
-			};
-
-			console.log(this);
-
+			this.data.value = { ...res.auth_login };
+			await this.fetchUser(email);
+			useGqlToken(res.auth_login?.access_token!);
+			this.authTokenCookie.value = res.auth_login?.access_token!;
+			this.refreshTokenCookie.value = res.auth_login?.refresh_token!;
+			this.lastUserCookie.value = this.user?.email!;
+			navigateTo("/app");
+			console.log(this.data.value.refresh_token);
 		} catch (err: any) {
 			this.resetFields();
 			console.error(err?.["gqlErrors"][0].message ?? err);
 		}
 	}
 
-    public get user(){
-        return this.data.value?.currentUser;
-    }
-    public get isLoggedIn(){
-        return this.data.value?.accessToken != undefined;
-    }
+	public async tryContinueSession() {
+		if (
+			this.refreshTokenCookie.value != null &&
+			this.lastUserCookie.value != null &&
+			this.authTokenCookie.value != null
+		) {
+			useGqlToken(this.authTokenCookie.value);
+			await this.sendRefreshRequest(this.refreshTokenCookie.value);
+		}
+	}
+
+	private async fetchUser(email: string) {
+		let res = await GqlGetUserByMail({ email: email });
+		this.data.value.currentUser = { ...res.users?.[0] };
+	}
+
+	private async sendRefreshRequest(refreshToken: string) {
+		try {
+			console.log("REFRESHING");
+			let res = await GqlRefreshToken({
+				refresh_token: refreshToken,
+			});
+			console.log(this.refreshTokenCookie.value);
+			console.log("OLD", this.data.value.access_token);
+			console.log("NEW", res.auth_refresh?.access_token);
+			this.data.value = { ...res.auth_refresh };
+			await this.fetchUser(this.lastUserCookie.value!);
+			this.refreshTokenCookie.value = res.auth_refresh?.refresh_token!;
+		} catch (err: any) {
+			console.log(err);
+		}
+	}
+
+	public get user() {
+		return this.data.value?.currentUser;
+	}
+
+	public get isLoggedIn() {
+		return this.data.value?.access_token != undefined;
+	}
+
 	public async sendLogoutRequest() {
-		if (this.data.value.accessToken) {
-			let res = await GqlLogout({ token: this.data.value.accessToken });
+		if (this.data.value.access_token) {
+			let res = await GqlLogout({ token: this.data.value.access_token });
 			if (res.auth_logout) {
 				console.log(res);
 				this.resetFields();
@@ -45,14 +92,15 @@ class AuthService {
 			}
 		}
 	}
+
 	private resetFields() {
-		this.data.value.accessToken = undefined;
+		this.data.value.access_token = undefined;
 		this.data.value.currentUser = undefined;
-		this.data.value.tokenExpiration = undefined;
-		this.data.value.refreshToken = undefined;
+		this.data.value.expires = undefined;
+		this.data.value.refresh_token = undefined;
 	}
 }
 export const useAuth = () => {
-    useCookie("token", {sameSite: "strict"})
+	useCookie("token", { sameSite: "strict" });
 	return new AuthService();
 };
