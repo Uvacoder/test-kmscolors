bb14bf9656787f5de88479f19d885f559eefb680
just-maik <maik.jonietz@googlemail.com>
2022-11-29 18:45:30 +0000

    we got docs now Pog

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -26,11 +26,21 @@ class AuthService {
 
 	public attemptingRestore = useState<boolean>("restoreState", () => false);
 
+	/**
+	 * Sets the cookies
+	 * @param refresh_token
+	 * @param email
+	 */
 	private setCookies(refresh_token: string | null, email: string | null) {
 		this.refreshTokenCookie.value = refresh_token;
 		this.lastUserCookie.value = email;
 	}
 
+	/**
+	 * tries to log-in using given credentials
+	 * @param {string} email - directus e-mail
+	 * @param {string} password - directus password
+	 */
 	public async sendLoginRequest(email: string, password: string) {
 		try {
 			let res = await GqlLogin({ email: email, password: password });
@@ -45,6 +55,9 @@ class AuthService {
 		}
 	}
 
+	/**
+	 * Sends a refresh request if cookies are set
+	 */
 	public async tryContinueSession() {
 		if (
 			this.refreshTokenCookie.value != null &&
@@ -55,11 +68,19 @@ class AuthService {
 		}
 	}
 
+	/**
+	 * Fetches user by email
+	 * @param {string} email
+	 */
 	private async fetchUser(email: string) {
 		let res = await GqlGetUserByMail({ email: email });
 		this.data.value.currentUser = { ...res.users?.[0] };
 	}
 
+	/**
+	 * refreshes an access token using a refresh token
+	 * @param {string} refreshToken
+	 */
 	private async sendRefreshRequest(refreshToken: string) {
 		try {
 			let res = await GqlRefreshToken({
@@ -69,32 +90,39 @@ class AuthService {
 			await this.fetchUser(this.lastUserCookie.value!);
 			this.setCookies(res.auth_refresh?.refresh_token!, this.user?.email!);
 			this.attemptingRestore.value = false;
-			//useGqlToken(res.auth_refresh?.access_token!);
 			this.refreshTokenCookie.value = res.auth_refresh?.refresh_token!;
 			if (this.lastFailedNavigation.value!!) {
 				navigateTo(this.lastFailedNavigation.value);
 			}
-			if(restoreBlacklistRoutes.includes(useRoute().path)){
-				navigateTo("/")
+			if (restoreBlacklistRoutes.includes(useRoute().path)) {
+				navigateTo("/");
 			}
 		} catch (err: any) {
 			this.attemptingRestore.value = false;
 			this.resetFields();
 			this.setCookies(null, null);
-			//useGqlToken(null);
 			console.log(err);
 		}
-		this.lastFailedNavigation.value = null
+		this.lastFailedNavigation.value = null;
 	}
 
+	/**
+	 * Gets the current user
+	 */
 	public get user() {
 		return this.data.value?.currentUser;
 	}
 
+	/**
+	 * Checks the login status
+	 */
 	public get isLoggedIn() {
 		return this.data.value?.access_token != undefined;
 	}
 
+	/**
+	 * Sends a logout request
+	 */
 	public async sendLogoutRequest() {
 		if (this.data.value.access_token) {
 			let res = await GqlLogout({ token: this.data.value.access_token });
@@ -108,6 +136,9 @@ class AuthService {
 		}
 	}
 
+	/**
+	 * resets the auth state
+	 */
 	private resetFields() {
 		this.data.value.access_token = undefined;
 		this.data.value.currentUser = undefined;
@@ -115,6 +146,11 @@ class AuthService {
 		this.data.value.refresh_token = undefined;
 	}
 }
+
+/**
+ * Provides basic authentication functionalities
+ * @returns a shared instance of AuthService
+ */
 export const useAuth = () => {
 	return new AuthService();
 };
